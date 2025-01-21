561aae55f1aa9908b528e71d09477828123c1576
sebas777 <sebastiancast06@hotmail.com>
2025-01-21 18:13:22 -0500

    [sc] calculadora

---
+++ nivel-1/calculadora/operaciones.js
@@ -0,0 +1,15 @@
+exports.suma = function (number1, number2) {
+    return number1 + number2;
+}
+
+exports.restar = function (number1, number2) {
+    return number1 - number2;
+}
+
+exports.multiplicar = function (number1, number2) {
+    return number1 * number2;
+}
+
+exports.dividir = function (number1, number2) {
+    return number1 / number2;
+}
